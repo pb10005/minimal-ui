94e2d8b25f1cf580e22bbb3fb7e6179a59a428ad
pb10001 <atihsa.mihs.ne.ho.da@gmail.com>
2019-01-05 23:09:01 +0900

    Move folder

--- src/doc/main.js
+++ src/doc/main.js
@@ -1,7 +1,7 @@
 import Vue from "vue";
 import App from "./App.vue";
 
-import MinimalUI from "./index.js";
+import MinimalUI from "../index.js";
 Vue.use(MinimalUI);
 
 Vue.config.productionTip = false;
